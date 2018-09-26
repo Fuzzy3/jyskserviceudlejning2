f9f89417a542219cc7dec21702ff8458d461a912
Søren Oest Balmer <soren.oest.balmer@systematic.com>
2018-09-26 15:59:31 +0200

    frontpage updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
