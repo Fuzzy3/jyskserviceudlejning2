6d3a6964a8b708e023ec14ac0710c6771cb5e48c
Fuzzy3 <oestjacobsen93@gmail.com>
2019-01-11 18:01:58 +0100

    Styling bestilling

--- src/app/bestilling/bestillingsliste/bestillingsliste.component.ts
+++ src/app/bestilling/bestillingsliste/bestillingsliste.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { IBestilling, Bestilling } from '../model/bestilling.model';
+import { Produkt } from '../model/produkt.model';
 
 @Component({
   selector: 'app-bestillingsliste',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BestillingslisteComponent implements OnInit {
 
+  @Input() bestilling: IBestilling;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generateOrderList(): Bestilling[] {
+    const produkter: Bestilling[] = [];
+    Object.keys(this.bestilling).forEach(key => {
+        const value = this.bestilling[key]; /* Use key, value here */
+        if(value.antal > 0) {
+          produkter.push(value);
+        }
+    });
+    return produkter;
   }
 
 }
