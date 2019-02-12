eef3258b0ce6e01972487ea970647fc5a6378444
Søren Oest Balmer <oestjacobsen93@gmail.com>
2019-02-12 20:46:37 +0100

    Bestillingsiden har fået redesign

--- src/app/bestilling/bestilling.component.ts
+++ src/app/bestilling/bestilling.component.ts
@@ -1,8 +1,7 @@
-import { element } from 'protractor';
 import { BestillingInfo } from './model/bestillingInfo.model';
 import { Bestilling, IBestilling } from './model/bestilling.model';
 import { ProduktSerie } from './model/produktSerie.model';
-import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, AfterViewInit, EventEmitter } from '@angular/core';
 import { ProductService } from './services/product.service';
 import { Produkt } from './model/produkt.model';
 import { NgForm } from '@angular/forms';
@@ -18,20 +17,37 @@ export class BestillingComponent implements OnInit, AfterViewInit {
   bestillingsListe: IBestilling = {};
   menuPosition: any;
   @ViewChild('stickyMenu') menuElement: ElementRef;
+  @ViewChild('fullHeight') fullHeightElement: ElementRef;
+  stickyBot: Boolean = false;
   sticky: Boolean = false;
+  innerHeight: any;
   @HostListener('window:scroll', ['$event']) handleScroll() {
     const windowScroll = window.pageYOffset;
-        if (windowScroll >= this.menuPosition) {
-            console.log('true');
-            this.sticky = true;
-          } else {
-            console.log('false');
-            this.sticky = false;
+        if ((windowScroll >= this.menuPosition) && (windowScroll <= this.calcStopperY())) {
+          this.sticky = true;
+          this.stickyBot = false;
+        } else if (windowScroll > this.calcStopperY()) {
+          this.sticky = false;
+          this.stickyBot = true;
+        } else {
+          this.sticky = false;
+          this.stickyBot = false;
         }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerHeight = window.innerHeight;
   }
 
   ngAfterViewInit() {
     this.menuPosition = 213;
+    console.log(this.fullHeightElement);
+  }
+
+  private calcStopperY() {
+    const fullHeight = this.fullHeightElement.nativeElement.clientHeight;
+    return fullHeight + this.menuPosition - (innerHeight * 0.8);
   }
 
 
@@ -50,6 +66,20 @@ export class BestillingComponent implements OnInit, AfterViewInit {
       this.bestillingsListe[id] = null;
     }
     this.bestillingsListe[id] = {antal: amount, produkt: this.productSerier[produktSerieId].produkter[produktId]};
+    this.productService.setBestillingsliste(this.bestillingsListe);
+  }
+
+  onKuvertProductAdded(amount: number, productid: string, produktNavn: string, pris: number) {
+    const id = productid;
+    if (amount === 0) {
+      this.bestillingsListe[id] = null;
+    }
+    const kuvert: Produkt = new Produkt();
+    kuvert.navn = produktNavn;
+    kuvert.pris = pris;
+    kuvert.billedURL = '';
+
+    this.bestillingsListe[id] = {antal: amount, produkt: kuvert};
     this.productService.setBestillingsliste(this.bestillingsListe);
   }
 
