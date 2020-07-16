9da563d869a9c9beacdc302c9fe8382f880c7b99
Søren Oest Balmer <soren.oest.jacobsen@systematic.com>
2020-07-16 15:14:30 +0200

    updated to use mail_receiver json with all mails

--- src/app/kontakt/kontakt.component.ts
+++ src/app/kontakt/kontakt.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { MailService } from '../bestilling/services/mail.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-kontakt',
@@ -8,11 +11,22 @@ import { Title } from '@angular/platform-browser';
 })
 export class KontaktComponent implements OnInit {
 
-  constructor(titleService: Title) {
+  constructor(titleService: Title, private mailService: MailService) {
     titleService.setTitle("Kontakt jyskserviceudlejning her telefon mail")
   }
 
   ngOnInit() {
   }
+
+  getMailSender(): Observable<String> {
+    return this.mailService.getMailReceiver().pipe(
+      map(value => 'mailto:' + value)
+    );
+  }
+
+  getMail(): Observable<String> {
+    return this.mailService.getMailReceiver();
+  }
+
 
 }
